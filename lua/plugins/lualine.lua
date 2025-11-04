eb98d9245e6dcb5ab42cb590253ea123d588667e
Edgar Figueroa <efiguerc@gmail.com>
2025-11-03 16:43:26 -0800

    remove custom lualine sections

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -282,30 +282,6 @@ return  {
           globalstatus = false,
           theme  = 'powerline',
         },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = {
-            git_component.component, -- Our custom git status
-          },
-          lualine_c = {
-            { "filename", path = 1 },
-          },
-          lualine_x = {
-            "encoding",
-            { "fileformat", symbols = { unix = "LF", dos = "CRLF", mac = "CR" } },
-            "filetype",
-          },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
-        },
       })
     end,
   },
